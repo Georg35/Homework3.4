b5d2c496dccd207ea9c17841d6618b18796c21c3
Георгий <georgijstenin@gmail.com>
2024-01-28 14:24:54 +0500

    ДЗ 3.5 завершена

---
+++ src/main/java/pro/sky/homework34/model/Avatar.java
@@ -0,0 +1,27 @@
+package pro.sky.homework34.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    @Lob
+    private byte[] data;
+    @OneToOne
+    private Student student;
+}
